5a5c82ab625f3e824b322986880d2f6350a80701
Charles Crawford <charliec364@gmail.com>
2016-07-11 08:59:22 -0700

    simplify index

--- app/index.jsx
+++ app/index.jsx
@@ -1,60 +1,14 @@
-import React from 'react'
-import {render} from 'react-dom'
+import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 
-var styles = {
-  slider: {
-    border: 'solid',
-    color: 'green'
-  }
-}
-
-class App extends React.Component {
-  constructor(){
-    super();
-    this.state = {
-      red: 0,
-      green: 0,
-      blue: 0
-    }
-    this.update = this.update.bind(this)
-  }
-  update(e){
-    this.setState({
-      red: ReactDOM.findDOMNode(this.refs.red.refs.inp).value,
-      green: ReactDOM.findDOMNode(this.refs.green.refs.inp).value,
-      blue: ReactDOM.findDOMNode(this.refs.blue.refs.inp).value
-    })
-  }
+class App extends Component {
   render(){
     return (
-      <div style={{topMargin: 'solid'}}>
-        <Slider ref="red" update={this.update} />
-        {this.state.red}
-        <br />
-        <Slider ref="green" update={this.update} />
-        {this.state.green}
-        <br />
-        <Slider ref="blue" update={this.update} />
-        {this.state.blue}
-        <br />
+      <div>
+        <p>Hello!</p>
       </div>
     );
   }
 }
-
-class Slider extends React.Component {
-  render(){
-    return (
-        <div>
-        <input style={styles.slider} ref="inp" type="range"
-          min="0"
-          max="255"
-          onChange={this.props.update} />
-        </div>
-    );
-  }
-}
-
 
 ReactDOM.render(<App />, document.getElementById('app'));
